82eba173ec373e44e181da405902770eab03a21f
Mohamed Alaa <ma722072207220@gmail.com>
2023-05-01 14:28:20 +0300

    updated view

--- comments/views.py
+++ comments/views.py
@@ -1,5 +1,5 @@
 from rest_framework.response import Response
-from rest_framework import status, generics
+from rest_framework import status, generics, views
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Comments, Product
 from .serializer import AddCommentSerializer, AllCommentsSerializer
@@ -7,10 +7,9 @@ from django.shortcuts import get_object_or_404
 from project.serializer_error import serializer_error
 
 
-class CommentsView(generics.ListCreateAPIView):
+class CommentsView(views.APIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = AllCommentsSerializer
-    queryset = Comments.objects.select_related('product', 'user')
 
     def post(self, request, pk=None):
         data = request.data
@@ -21,3 +20,10 @@ class CommentsView(generics.ListCreateAPIView):
             return Response({"message": "Comment added successfully"}, status=status.HTTP_201_CREATED)
         else:
             return serializer_error(serializer)
+
+    def get(self, request,  pk=None):
+        product = get_object_or_404(Product, pk=pk)
+        comments = product.comments_product.select_related(
+            'product', 'user').all()
+        serializer = self.serializer_class(comments, many=True).data
+        return Response(serializer, status=status.HTTP_201_CREATED)
