ed9c37d6383b50d7189b3e3620d05980587bfa8f
Mohamed Alaa <ma722072207220@gmail.com>
2023-05-08 14:34:16 +0300

    updated

--- project/settings.py
+++ project/settings.py
@@ -109,15 +109,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'quarter',
-        'USER': 'mohamed',
-        'PASSWORD': '722072207220',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
