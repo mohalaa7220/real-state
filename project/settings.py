981bda2d661613d3891299c58fdb8acd5c3e0504
Mohamed Alaa <ma722072207220@gmail.com>
2023-05-24 22:50:12 +0300

    updated

--- project/settings.py
+++ project/settings.py
@@ -68,7 +68,8 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    'https://dashboard.render.com'
+    'https://dashboard.render.com',
+    'https://render.com/'
 ]
 
 CORS_ALLOW_METHODS = [
